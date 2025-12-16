efa8431246f4a0e9eb1c5a7de33b1c6fdd61eed3
narakorn <narakornngaongam45@gmail.com>
2025-12-16 13:23:18 +0700

    add new line without enter insert mode

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>o", ":put _<CR>", { desc = "Blank line below" })
+vim.keymap.set("n", "<leader>O", ":put! _<CR>", { desc = "Blank line above" })
